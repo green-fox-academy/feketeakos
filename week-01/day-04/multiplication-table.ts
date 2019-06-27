159dcdb7e0f8e50400bd3ea803da91c14f887e56
Akos <feketeakos23@gmail.com>
2019-06-27 17:19:17 +0200

    ready

---
+++ week-01/day-04/multiplication-table.ts
@@ -0,0 +1,6 @@
+let number: number = 8;
+let a: number = 0;
+while (a < 10) {
+    a = a + 1;
+    console.log(a , '*' , number , '=' , a * number) ;
+}
---
+++ week-01/day-04/print-even.ts
@@ -0,0 +1,4 @@
+let inputNumber: number = 0
+for (let inputNumber: number = 0 ; inputNumber < 500 ; inputNumber = inputNumber + 2) {
+        console.log(inputNumber);
+}
