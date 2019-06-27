505cf73b67f786c0473123df008f5b804eb91723
Akos <feketeakos23@gmail.com>
2019-06-27 11:20:08 +0200

    done

---
+++ week-01/day-04/loops.ts
@@ -0,0 +1,10 @@
+let times: number = 0;
+
+while(times < 10) {
+    console.log("nota", times);
+    times ++;
+}
+
+for(let iterator: number = 0; iterator < 100 ; iterator ++) {
+    console.log(iterator);
+}
