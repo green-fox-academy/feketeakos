aee119e836151e78eb03fe19c1714e9e1678e408
Akos <feketeakos23@gmail.com>
2019-06-27 15:13:02 +0200

    ready

---
+++ week-01/day-04/variable-mutation.ts
@@ -0,0 +1,35 @@
+let a: number = (3 + 10);
+console.log(a);
+let b: number = 100 - 7;
+console.log(b);
+let c: number = 44 * 2;
+console.log(c);
+let d: number = 125 / 5;
+console.log(d);
+let e: number = 8 ** 3;
+console.log(e);
+
+let f1: number = 123;
+let f2: number = 345;
+let f1Bigger: boolean = f1 > f2;
+console.log(f1Bigger)
+
+let g1: number = 350;
+let g2: number = 200;
+let g2Bigger: boolean = g2 * 2 > g1;
+console.log(g2Bigger);
+
+let h: number = 1357988018575474;
+let divisor: number = 11;
+console.log(h % divisor == 0);
+
+let i1: number = 10;
+let i2: number = 3;
+let highOrNot: boolean = (i1 > i2 **2 ) && (i1 < i2 ** 3);
+console.log(highOrNot);
+
+let j: number = 1521;
+console.log(j % 3 == 0 && j % 5 == 0);
+
+
+
