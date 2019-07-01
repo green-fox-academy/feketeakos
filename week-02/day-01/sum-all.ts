5f8dfb84ed2159564609ff35bb33fd7990f2679f
Ákos Fekete <feketeakos23@users.noreply.github.com>
2019-07-01 17:36:04 +0200

    ready

--- week-02/day-01/sum-all.ts
+++ week-02/day-01/sum-all.ts
@@ -1,5 +1,12 @@
 'use strict';
 let ai: number[] = [3, 4, 5, 6, 7];
+let total = 0;
+for(let i: number = 0; i < ai.length; i++){
+total = total + ai[i];}
+console.log(total)
 
-ai.reduce(function(total, 0);
-console.log(function);
+/*let total = ai.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue;
+  });
+console.log(total);
+*/
