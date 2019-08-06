67b4eb3f865ad83168f67946456f40e6b31bb3a7
Ákos Fekete <feketeakos23@users.noreply.github.com>
2019-08-06 17:26:13 +0200

    done

---
+++ week-06/day-02/index4.js
@@ -0,0 +1,8 @@
+if (document.querySelectorAll('p')[3].className === 'dolphin') {
+    document.querySelector('.apple').textContent = 'pear';
+}
+if (document.querySelectorAll('p')[0].className === 'apple') {
+    document.querySelector('.cat').textContent === 'dog';
+}
+document.querySelector('.apple').classList.add('red');
+document.querySelector('.balloon').style.borderRadius = '3%';
