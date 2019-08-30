ea2d423d0d3ea53eb7528f54481d2bc967ca9644
Ákos Fekete <feketeakos23@users.noreply.github.com>
2019-08-30 11:22:38 +0200

    all done and clickable

--- Home practice/Gallery-app/public/index.js
+++ Home practice/Gallery-app/public/index.js
@@ -8,8 +8,8 @@ const picArray = [
     },
     {
         path: 'https://resize.hswstatic.com/w_907/gif/hedgehog-pet.jpg',
-        title: 'Sün',
-        text: 'A sünfélék az emlősök osztályának és az Eulipotyphla rendjének egyik családja. A régebbi rendszertani besorolások szerint a rovarevők rendjébe, aztán pedig rövid ideig a saját sünalakúak rendjébe tartozott. '
+        title: 'Fahéj',
+        text: 'Jó nagyokat szarik, általában nehezen lemosható helyekre. Mi mást mondhatnék? Egy szargyár. '
     },
     {
         path: 'https://mult-kor.hu/image/article/main/.630x1260/38480.jpg',
@@ -33,20 +33,100 @@ const picArray = [
     },
     {
         path: 'https://cdn.nwmgroups.hu/s/img/i/1505/20150518kacsacsoru-emlos1.jpg',
-        title: 'Kacsacsőrű emlős',
-        text: 'A kacsacsőrű emlős (Ornithorhynchus anatinus) az emlősök (Mammalia) osztályának a kloákások (Monotremata) rendjébe és a kacsacsőrű emlősök (Ornithorhynchidae) családjába tartozó egyetlen élő faj.'
+        title: 'Platypus',
+        text: 'A kacsacsőrű emlős (Ornithorhynchus anatinus) a kacsacsőrű emlősök (Ornithorhynchidae) családjába tartozó egyetlen élő faj.'
     },
 ];
-function show_image(imgNumber) {
-    var img = document.createElement("img");
-    img.src = picArray[imgNumber].path;
+let imageNumber = picArray.indexOf(0) + 1;
+let img = document.createElement("img");
+document.querySelector('h2').textContent = `${picArray[0].title}`;
+document.querySelector('p').textContent = `${picArray[0].text}`;
+
+function show_image(imageNumber) {
+    img.src = picArray[imageNumber].path;
     img.width = 711;
     img.height = 400;
     document.getElementsByClassName("flex-picture")[0].appendChild(img);
 }
-const arrowR = document.getElementsByTagName("li")[0]
-arrowR.addEventListener('click', function() {
-    show_image(0)
+
+const imgList = document.querySelector('.thumbnails');
+
+function showThumbs() {
+    for (let i = 0; i < picArray.length; i++) {
+        let thumb = document.createElement('img');
+        thumb.src = picArray[i].path;
+        thumb.className = 'thumbnails'
+        thumb.width = 771 / picArray.length;
+        thumb.height = 84;
+        imgList.appendChild(thumb);
+        thumb.addEventListener('click', function () {
+            show_image(i);
+            document.querySelector('h2').textContent = `${picArray[i].title}`;
+            document.querySelector('p').textContent = `${picArray[i].text}`;
+            /* ('.selected').remo
+            thumb.classList.add('selected') */
+            /*  thumb.classList.toggle("selected"); */
+            /*  document.querySelectorAll.classList.remove('selected');
+              thumb.classList.add('selected');  */
+        });
+    }
+}
+
+
+function previousImage() {
+    if (imageNumber === 0) {
+        img.src = picArray[picArray.length - 1].path;
+        img.width = 711;
+        img.height = 400;
+        let item = document.getElementsByClassName("flex-picture")[0]
+        item.replaceChild(img, img);
+        document.querySelector('h2').textContent = `${picArray[picArray.length - 1].title}`;
+        document.querySelector('p').textContent = `${picArray[picArray.length - 1].text}`;
+        imageNumber = picArray.length - 1
+    } else {
+        img.src = picArray[imageNumber - 1].path;
+        img.width = 711;
+        img.height = 400;
+        let item = document.getElementsByClassName("flex-picture")[0]
+        item.replaceChild(img, img);
+        document.querySelector('h2').textContent = `${picArray[imageNumber - 1].title}`;
+        document.querySelector('p').textContent = `${picArray[imageNumber - 1].text}`;
+        imageNumber--;
+    }
+}
+
+function nextImage() {
+    if (imageNumber === picArray.length - 1) {
+        img.src = picArray[0].path;
+        img.width = 711;
+        img.height = 400;
+        let item = document.getElementsByClassName("flex-picture")[0]
+        item.replaceChild(img, img);
+        document.querySelector('h2').textContent = `${picArray[0].title}`;
+        document.querySelector('p').textContent = `${picArray[0].text}`;
+        imageNumber = 0;
+    } else {
+        img.src = picArray[imageNumber + 1].path;
+        img.width = 711;
+        img.height = 400;
+        let item = document.getElementsByClassName("flex-picture")[0]
+        item.replaceChild(img, img);
+        document.querySelector('h2').textContent = `${picArray[imageNumber + 1].title}`;
+        document.querySelector('p').textContent = `${picArray[imageNumber + 1].text}`;
+        imageNumber++;
+    }
+}
+
+const arrowL = document.getElementById("0");
+arrowL.addEventListener('click', function () {
+    previousImage()
 });
 
-show_image(5)
+const arrowR = document.getElementById("1");
+arrowR.addEventListener('click', function () {
+    nextImage()
+});
+
+
+show_image(0)
+showThumbs()
