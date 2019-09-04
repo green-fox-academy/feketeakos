b68e985826dfed9617265d0c3f959b49cd333613
Ákos Fekete <feketeakos23@users.noreply.github.com>
2019-09-04 12:58:33 +0200

    finished

--- Home practice/Bomb-defuser/public/index.js
+++ Home practice/Bomb-defuser/public/index.js
@@ -5,6 +5,7 @@ let defuseButton = document.getElementsByClassName('defuse')[0];
 let timeField = document.getElementsByClassName('timer')[0];
 let img = document.createElement("img");
 let timer = 11;
+let myInterval;
 
 function counter() {
     if (timer > 0) {
@@ -12,6 +13,7 @@ function counter() {
         timeField.textContent = `Time remaining: ${timer} sec`;
     }
     if (timer === 0) {
+        timeField.textContent = 'The World has exploded! Kim Jong Un likes this.'
         img.src = 'https://media.giphy.com/media/l3vR1tookIhM8nZJu/giphy.gif';
         img.height = 200;
         img.width = 300;
@@ -20,26 +22,25 @@ function counter() {
 }
 
 function start() {
+    clearInterval(myInterval);
+    myInterval = setInterval(counter, 1000);
     timer = 11;
-    setInterval(function () { 
-        counter() 
-    }, 1000);
     img.src = 'https://media.giphy.com/media/l0MYz5mGpy0vLGnbW/giphy.gif';
     img.height = 200;
     img.width = 300;
     bomb.appendChild(img);
-
-
+    startButton.textContent = 'RESTART'
 }
+
 function defuse() {
+    timeField.textContent = 'Oooh, that was hot! Congrats!';
+    clearInterval(myInterval);
     img.src = 'https://media.giphy.com/media/4UEyopJPeJEze/giphy.gif';
     img.height = 200;
     img.width = 300;
-    bomb.removeChild(img);
-    bomb.appendChild(img);
-    clearInterval()
+    bomb.replaceChild(img);
+    timer = 11;
 }
-
 
 startButton.addEventListener('click', start)
 defuseButton.addEventListener('click', defuse);
