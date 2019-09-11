964857980ae92313c26c4ca197f594e203794fe6
Ákos Fekete <feketeakos23@users.noreply.github.com>
2019-09-11 17:38:33 +0200

    show tracks work

--- week-10/audio_player/public/index.js
+++ week-10/audio_player/public/index.js
@@ -1,10 +1,15 @@
 'use strict';
 
 let URL = '/playlists'
+let TRACKS = '/alltracks'
+
+fetch(TRACKS)
+    .then(res => res.json())
+    .then(res => alltracks(res))
 
 fetch(URL)
-.then(res => res.json())
-.then(res => render(res))
+    .then(res => res.json())
+    .then(res => render(res))
 
 function render(playlists) {
     for (let i = 0; i < playlists.length; i++) {
@@ -17,32 +22,48 @@ function render(playlists) {
         newPlaylist.appendChild(trackName);
         newPlaylist.appendChild(addButton);
     }
-}
+};
 
-let addButton = document.getElementsByClassName('new')[0]
+function alltracks(alltracks) {
+    for (let i = 0; i < alltracks.length; i++) {
+        let newTrack = document.createElement('h4');
+        document.getElementsByClassName('track_title')[0].appendChild(newTrack);
+        let trackName = document.createElement('h4');
+        trackName.innerHTML = alltracks[i];
+        newTrack.appendChild(trackName);
+    }
+};
+
+let addButton = document.getElementsByClassName('new')[0];
+let deleteButton = document.getElementsByClassName('delete')[0];
 
 addButton.addEventListener('click', addPlaylist);
+deleteButton.addEventListener('click', deletePlaylist);
 
 function addPlaylist() {
     const playListName = prompt('Provide a playlist name');
-    fetch('/playlists', { 
+    fetch('/playlists', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({playListName: playListName})})  
-    .then(response => response.text())
-    .then(console.log)
+        body: JSON.stringify({ playListName: playListName })
+    })
+        .then(response => response.text())
+        .then(console.log)
 }
 
 function deletePlaylist() {
-    const playListName = prompt('Provide a playlist ID');
-    fetch('/playlists', { 
+    const playListID = prompt('Provide a playlist ID');
+    fetch('/playlists/:id', {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({playListName: playListName})})  
-    .then(response => response.text())
-    .then(console.log)
+        body: JSON.stringify({ playListID: playListID })
+    })
+        .then(response => response.text())
+        .then(console.log)
 }
+
+
--- week-10/audio_player/server.js
+++ week-10/audio_player/server.js
@@ -2,7 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const mysql = require('mysql');
+const fs = require('fs');
 
+app.get('/alltracks', (req, res) => {
+  const files = fs.readdirSync('./music');
+  res.send(files).body
+});
+/* var fs = require('fs');
+var mm = require('musicmetadata');
+ 
+var parser = mm(fs.createReadStream('./public/test.mp3'), function (err, metadata) {
+  if (err) throw err;
+  console.log(metadata);
+});
+ */
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -24,8 +37,8 @@ app.get('/playlist-tracks', (req, res) => {
 });
 
 app.get('/playlists', (req, res) => {
-  connection.query('SELECT * FROM playlist;', function(error,result) {
-    if(error) {
+  connection.query('SELECT * FROM playlist;', function (error, result) {
+    if (error) {
       console.log(error)
     } else {
       res.send(result)
@@ -38,10 +51,10 @@ app.post('/playlists', (req, res) => {
   connection.query('INSERT INTO playlist (Title) VALUES (?)', playList)
 })
 
-/* app.delete('/playlists/:id', (req, res) =>{
-  const playlist = req.body.playListName
-  connection.query('DELETE FROM playlist WHERE Title=')
-} */
+app.delete('/playlists/:id', (req, res) => {
+  const playListID = req.body.playListID
+  connection.query('DELETE FROM playlist WHERE Id=' + playListID)
+});
 /* 
 app.post('/send', (req, res) => {
   res.send('okay!');
